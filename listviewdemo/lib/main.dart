ab7a8a24605c94ada4f4a455925deef4df8d1a20
Rootshell <bcairns@voidrealms.com>
2018-01-09 13:23:17 -0500

    First Commit

---
+++ listviewdemo/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+
+  List<bool> _data = new List<bool>();
+
+  @override
+  void initState() {
+    setState((){
+      for(int i = 0; i < 1000; i++) {
+        _data.add(false);
+      }
+    });
+  }
+
+  void _onChange(bool value, int index) {
+    setState((){
+      _data[index] = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('ListView Demo'),
+      ),
+      body: new ListView.builder(
+          itemCount: _data.length,
+          itemBuilder: (BuildContext context, int index){
+          return new Card(
+            child: new Container(
+              padding: new EdgeInsets.all(5.0),
+              child: new Column(
+                children: <Widget>[
+                  new Text(('This is item ${index}')),
+                  new CheckboxListTile(
+                      value: _data[index],
+                      controlAffinity: ListTileControlAffinity.leading,
+                      title: new Text('Click me item ${index}'),
+                      onChanged: (bool value){_onChange(value, index);},
+                  )
+                ],
+              ),
+            ),
+          );
+          }
+      ),
+    );
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+
+Code for video tutorial
+
+Title: Flutter 34 - Listview
+Published: 1/9/2018 1:15 AM
+Link: https://youtu.be/yKhn6GGaaZU
+
+Description: Learn how to use the Listview
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
